57309763de314687bbf8880993bdb077519f3aed
alexandre <lutosaalexandre@gmail.com>
2022-07-17 00:25:27 -0300

    feat: implemented assetBuy path when client already has asset on wallet

--- src/services/assetCustomer.service.js
+++ src/services/assetCustomer.service.js
@@ -5,17 +5,44 @@
 //Adicionar se ativo não presente
 const {Asset_Customers: assetCustomers, Assets, Customers} = require('../db/models')
 
-const getCustomerAssets = async (clientId) => {
+const getCustomerAssets = async (customerId) => {
   const customerAssets = await Customers.findOne({
-    where: { id: clientId },
+    where: { id: customerId },
     include:[{model: Assets, as: 'assets', through: {attributes: ['quantity']}}]
   })
   return customerAssets
 }
 
+const checkAssetInWallet = ({ assets },codAtivo) => {
+  const foundAsset = assets.find(({dataValues}) => dataValues.asset_code === codAtivo)
+  return foundAsset
+}
+
+const updateAssetQuantity = async ({dataValues}, qtdeAtivo, codCliente) => {
+  const {id: assetId} = dataValues
+  const { dataValues: assetDetails } = dataValues.Asset_Customers
+  const newQuantity = assetDetails.quantity + qtdeAtivo
+  await assetCustomers.update({ quantity: newQuantity },
+    {
+      where: {
+        customer_id: codCliente,
+        asset_id: assetId
+      }
+    })
+  return newQuantity 
+}
+
+const createAssetInWallet = async () => {
+  return true
+}
+
 const buyOrder = async ({ codCliente, codAtivo, qtdeAtivo }) => {
-  const customerAssets = getCustomerAssets(codCliente)
-  return customerAssets
+  const customerAssets = await getCustomerAssets(codCliente)
+  const assetInWallet = checkAssetInWallet(customerAssets, codAtivo)
+  const newAssetQuantity = assetInWallet ?
+    await updateAssetQuantity(assetInWallet, qtdeAtivo, codCliente) : await createAssetInWallet()
+  const response = {codCliente, codAtivo, qtdeAtivo: newAssetQuantity}
+  return response
 }
 
 module.exports = {buyOrder}
