b7ec545c34a22b6473b43032a80c5eac44978480
alexandre <lutosaalexandre@gmail.com>
2022-07-17 00:42:25 -0300

    feat: implemented createAssetInWallet method

--- src/services/assetCustomer.service.js
+++ src/services/assetCustomer.service.js
@@ -4,6 +4,7 @@
 //Somar se ativo já presente
 //Adicionar se ativo não presente
 const {Asset_Customers: assetCustomers, Assets, Customers} = require('../db/models')
+const assetService = require('./asset.service')
 
 const getCustomerAssets = async (customerId) => {
   const customerAssets = await Customers.findOne({
@@ -18,7 +19,7 @@ const checkAssetInWallet = ({ assets },codAtivo) => {
   return foundAsset
 }
 
-const updateAssetQuantity = async ({dataValues}, qtdeAtivo, codCliente) => {
+const updateAssetQuantity = async ({dataValues}, {qtdeAtivo, codCliente}) => {
   const {id: assetId} = dataValues
   const { dataValues: assetDetails } = dataValues.Asset_Customers
   const newQuantity = assetDetails.quantity + qtdeAtivo
@@ -32,15 +33,20 @@ const updateAssetQuantity = async ({dataValues}, qtdeAtivo, codCliente) => {
   return newQuantity 
 }
 
-const createAssetInWallet = async () => {
-  return true
+const createAssetInWallet = async ({codAtivo, codCliente, quantity}) => {
+  const assetData = await assetService.getByName(codAtivo)
+  console.log(assetData)
+  //const newCustomerAssetRecord = {customer_id: codCliente, asset_id: assetData, quantity}
+  //const insertedRecord = await assetCustomers.create(newCustomerAssetRecord)
+  return assetData
 }
 
-const buyOrder = async ({ codCliente, codAtivo, qtdeAtivo }) => {
+const buyOrder = async (payload) => {
+  const { codCliente, codAtivo, qtdeAtivo } = payload
   const customerAssets = await getCustomerAssets(codCliente)
   const assetInWallet = checkAssetInWallet(customerAssets, codAtivo)
   const newAssetQuantity = assetInWallet ?
-    await updateAssetQuantity(assetInWallet, qtdeAtivo, codCliente) : await createAssetInWallet()
+    await updateAssetQuantity(assetInWallet, payload) : await createAssetInWallet(payload)
   const response = {codCliente, codAtivo, qtdeAtivo: newAssetQuantity}
   return response
 }
