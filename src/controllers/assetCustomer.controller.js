c0812240ce0ca19ada15da838f9bd35932e95725
alexandre <lutosaalexandre@gmail.com>
2022-07-17 01:08:58 -0300

    refac: changes assetCustomer controller to use StatusCodes package

--- src/controllers/assetCustomer.controller.js
+++ src/controllers/assetCustomer.controller.js
@@ -1,7 +1,8 @@
+const { StatusCodes } = require('http-status-codes')
 const assetCustomerService = require('../services/assetCustomer.service')
 const buyOrder = async (req, res) => {
-  const customer = await assetCustomerService.buyOrder(req.body)
-  res.status(200).json(customer)
+  const newAssetCustomerData = await assetCustomerService.buyOrder(req.body)
+  res.status(StatusCodes.CREATED).json(newAssetCustomerData)
 }
 
 module.exports = {buyOrder}
--- src/router.js
+++ src/router.js
@@ -2,11 +2,7 @@ const Router = require("express");
 const assetCustomerController = require("./controllers/assetCustomer.controller");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-})
-
-router.get('/investimentos/compra', assetCustomerController.buyOrder)
+router.post('/investimentos/compra', assetCustomerController.buyOrder)
 
 
 module.exports = router;
