a87b68bee56d54024988ebdbf92c352eb0c4fcec
alexandre <lutosaalexandre@gmail.com>
2022-07-19 02:07:00 -0300

    docs: added reqBody to investimentos/comprar docs

--- src/router.js
+++ src/router.js
@@ -39,12 +39,20 @@ const router = Router();
   *    post:
   *      tags: [Investimentos]
   *      description: Realiza a operação de compra e retorna um objeto com dados do cliente, incluindo a nova quantidade de ações
+  *      parameters:
+  *      - in: body
+  *        name: body
+  *        required: true
+  *        schema:
+  *          $ref: '#/components/schemas/Investimento'
   *      responses:
   *        200:
   *          content:
   *            application/json:
   *              schema:
+  *                type: object
   *                $ref: '#/components/schemas/Investimento'
+  *      
 */
 
 router.post('/investimentos/compra', tokenValidator ,validateQuantity, assetCustomerController.buyOrder)
