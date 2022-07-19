705ff4a336ffd8d5b63b05d1976ed26c72758bab
alexandre <lutosaalexandre@gmail.com>
2022-07-19 17:58:30 -0300

    tests: checkingAccountController createDepositOrder method tests

---
+++ tests/unit/controllers/checkingAccountController.test.js
@@ -0,0 +1,38 @@
+const chai = require('chai')
+const sinon = require('sinon')
+chai.use(require('sinon-chai'))
+const { expect } = require('chai')
+const { StatusCodes } = require('http-status-codes')
+const financialDataApiRequests = require('../../../src/utils/financialDataApiRequests')
+const { Assets } = require('../../../src/db/models')
+const { checkingAccountController } = require('../../../src/controllers')
+const { checkingAccountService } = require('../../../src/services')
+
+describe('createDepositOrder tests', () => {
+  const response = {}
+  const request = {}
+  
+  beforeEach(() => {
+    sinon.stub(checkingAccountService, 'createDepositOrder')
+    request.body = {codCliente: 1, Valor: 500}
+    response.status = sinon.stub().returns(response)
+    response.json = sinon.stub().returns()
+  });
+
+  afterEach(() => {
+    sinon.restore()
+  });
+
+  it('should call checkingAccountService createDepositOrder method once with correct parameters', async () => {
+    await checkingAccountController.createDepositOrder(request, response)
+    expect(checkingAccountService.createDepositOrder.calledOnce).to.be.true;
+    expect(checkingAccountService.createDepositOrder.calledWith(request.body)).to.be.true;
+  });
+  
+  it('should return status code OK and an empty response', async () => {
+    await checkingAccountController.createDepositOrder(request, response)
+    expect(response.status).calledWith(StatusCodes.OK);
+    expect(response.json).calledWith()
+  });
+
+});
