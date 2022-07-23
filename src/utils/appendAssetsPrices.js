b6572ed66204428e83a62599cfab0609b327fc48
alexandre <lutosaalexandre@gmail.com>
2022-07-22 23:54:14 -0300

    fix: small correction to appendAssetsPrices function

--- src/utils/appendAssetsPrices.js
+++ src/utils/appendAssetsPrices.js
@@ -1,7 +1,9 @@
+const financialDataApiRequests = require('../utils/financialDataApiRequests');
 
 const appendAssetsPrices = async ({ assets }) => {
+
   const assetsWithPrice = await Promise.all(assets.map(async (asset) => {
-    const price = await requestPrice(asset);
+    const price = await financialDataApiRequests.getAssetPrice(asset.asset_code);
     asset.Asset_Customers.price = price;
     return asset;
   }));
