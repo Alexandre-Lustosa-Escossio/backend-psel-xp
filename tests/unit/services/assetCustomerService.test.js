aea62b3ba028acf3c745170fd958f5a18638b5f6
alexandre <lutosaalexandre@gmail.com>
2022-07-24 01:10:56 -0300

    tests: implemented assetCustomerService buyOrder method tests

--- tests/unit/services/assetCustomerService.test.js
+++ tests/unit/services/assetCustomerService.test.js
@@ -3,8 +3,6 @@ const sinon = require('sinon');
 chai.use(require('sinon-chai'));
 const { expect } = require('chai');
 const rewire = require('rewire'); 
-const { customerService } = require('../../../src/services')
-const { processBuyOrder } = require('../../../src/utils/orderBookMatching')
 const assetCustomerService = rewire("../../../src/services/assetCustomer.service.js");
 
 const payload = {
@@ -12,17 +10,77 @@ const payload = {
   "codAtivo": "xpbr31",
   "qtdeAtivo": 20
 }
-it('dsafsadfi', async () => {
-  const createAssetInWalletMock = sinon.stub().returns('xablau3')
+let createAssetInWalletMock
+let assembleOrderPayloadMock
+let getCustomerAssetByAssetCodeMock
+let processBuyOrderMock
+let transactionMock
+let revert
 
-  assetCustomerService.__set__({
-    "assembleOrderPayload": sinon.stub().returns('xablau'),
-    "customerService.getCustomerAssetByAssetCode": sinon.stub().returns(null),
-    "processBuyOrder": sinon.stub(),
-    "createAssetInWallet": createAssetInWalletMock
-  });
+describe('buyOrder tests', () => {
   
-  const response = await assetCustomerService.buyOrder(payload)
-  expect(response).to.equal('xablau3')
-  expect(createAssetInWalletMock.calledOnce).to.be.true
-})
+  
+  describe('when customer doesnt have the asset in wallet', () => {
+      
+    beforeEach(() => {
+      createAssetInWalletMock = sinon.stub().returns(payload)
+      assembleOrderPayloadMock = sinon.stub()
+      getCustomerAssetByAssetCodeMock = sinon.stub().returns(null)
+      processBuyOrderMock = sinon.stub()
+      revert = assetCustomerService.__set__({
+        "assembleOrderPayload": assembleOrderPayloadMock,
+        "customerService.getCustomerAssetByAssetCode": getCustomerAssetByAssetCodeMock,
+        "processBuyOrder": processBuyOrderMock,
+        "createAssetInWallet": createAssetInWalletMock
+        });
+      })
+      afterEach(() => {
+        revert()
+      });
+    
+    it('should return exact payload sent when customer doesnt have the asset in wallet', async () => {
+      const response = await assetCustomerService.buyOrder(payload)
+      expect(createAssetInWalletMock.calledOnce).to.be.true
+      expect(response).to.be.deep.equal(payload)
+    })
+    it('should call all stubbed methods once', async () => {
+      await assetCustomerService.buyOrder(payload)
+      expect(assembleOrderPayloadMock.calledOnce).to.be.true
+      expect(getCustomerAssetByAssetCodeMock.calledOnce).to.be.true
+      expect(processBuyOrderMock.calledOnce).to.be.true
+      expect(createAssetInWalletMock.calledOnce).to.be.true
+    })
+  })
+  
+  describe('when customer already has asset in wallet', () => {
+    const totalAssetsAfterSell = 30
+
+    beforeEach(() => {
+      assembleOrderPayloadMock = sinon.stub()
+      getCustomerAssetByAssetCodeMock = sinon.stub().returns('not null')
+      transactionMock = sinon.stub().returns(totalAssetsAfterSell)
+      revert = assetCustomerService.__set__({
+        "assembleOrderPayload": assembleOrderPayloadMock,
+        "customerService.getCustomerAssetByAssetCode": getCustomerAssetByAssetCodeMock,
+        "sequelize.transaction": transactionMock
+        });
+    })
+    
+    afterEach(() => {
+      revert()
+    });
+
+    it('should return payload with new quantity of assets', async () => {
+      const response = await assetCustomerService.buyOrder(payload)
+      expect(response).to.be.deep.equal({...payload, qtdeAtivo: totalAssetsAfterSell})
+    })
+
+    it('should call the stubbed methods once', async () => {
+      await assetCustomerService.buyOrder(payload)
+      expect(assembleOrderPayloadMock.calledOnce).to.be.true
+      expect(getCustomerAssetByAssetCodeMock.calledOnce).to.be.true
+      expect(transactionMock.calledOnce).to.be.true
+    })
+
+  });
+});
